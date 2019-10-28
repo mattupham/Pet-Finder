546950469f66c24bd3a3c9832c310d35fe7215f0
Matt Upham <mcupham@gmail.com>
2019-10-27 22:23:15 -0700

    Add function to retrieve pets from API on submit

--- src/SearchParams.js
+++ src/SearchParams.js
@@ -8,6 +8,17 @@ const SearchParams = () => {
   const [breeds, setBreeds] = useState([]);
   const [animal, AnimalDropdown] = useDropdown("Animal", "dog", ANIMALS);
   const [breed, BreedDropdown, setBreed] = useDropdown("Breed", "", breeds);
+  const [pets, setPets] = useState([]);
+
+  async function requestPets() {
+    const { animals } = await pet.animals({
+      location,
+      breed,
+      type: animal
+    });
+
+    setPets(animals || []);
+  }
 
   useEffect(() => {
     setBreeds([]);
@@ -15,14 +26,18 @@ const SearchParams = () => {
 
     pet.breeds(animal).then(({ breeds: apiBreeds }) => {
       const breedStrings = apiBreeds.map(({ name }) => name);
-      console.log("BREED STRINGS: ", breedStrings);
       setBreeds(breedStrings);
     }, console.error);
   }, [animal, setBreed, setBreeds]);
 
   return (
     <div className="search-params">
-      <form>
+      <form
+        onSubmit={e => {
+          e.preventDefault();
+          requestPets();
+        }}
+      >
         <label htmlFor="location">
           location
           <input
