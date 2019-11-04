a5acad5596fef1a50584cce516bdfd0ac39cfa3d
Matt Upham <mcupham@gmail.com>
2019-11-03 22:35:29 -0800

    Updated tests

--- src/__test__/SearchParams.test.js
+++ src/__test__/SearchParams.test.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { render, cleanup } from "@testing-library/react";
+import { render, cleanup, fireEvent } from "@testing-library/react";
 import pet, { ANIMALS, _breeds, _dogs } from "@frontendmasters/pet";
 import SearchParams from "../SearchParams";
 import { Test } from "tslint";
@@ -7,8 +7,20 @@ import { Test } from "tslint";
 afterEach(cleanup);
 
 test("SearchParams", async () => {
-  const { getByTestId } = render(<SearchParams />);
+  const { container, getByText, getByTestId } = render(<SearchParams />);
 
   const animalDropdown = getByTestId("use-dropdown-animal");
   expect(animalDropdown.children.length).toEqual(ANIMALS.length + 1);
+
+  expect(pet.breeds).toHaveBeenCalled();
+  const breedDropdown = getByTestId("use-dropdown-breed");
+  expect(breedDropdown.children.length).toEqual(_breeds.length + 1);
+
+  const searchResults = getByTestId("search-results");
+  expect(searchResults.textContent).toEqual("No Pets Found");
+  fireEvent(getByText("Submit"), new MouseEvent("click"));
+  expect(pet.animals).toHaveBeenCalled();
+  expect(searchResults.children.length).toEqaul(_dogs.length);
+
+  expect(container.firstChild).toMatchInlineSnapshot();
 });
